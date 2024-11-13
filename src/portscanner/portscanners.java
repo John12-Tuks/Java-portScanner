845a7cf3882ca29316975f3f810b4ca13e2b8107
John12-Tuks <153678573+John12-Tuks@users.noreply.github.com>
2024-11-13 15:25:17 +0200

    second commit
    added a method to scan port

--- src/portscanner/portscanners.java
+++ src/portscanner/portscanners.java
@@ -3,29 +3,92 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package portscanner;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.Socket;
 import java.util.Scanner;
+import javax.swing.*;
 
 /**
  *
  * @author TOKELO
  */
-public class portscanners {
-   public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-       System.out.println("Enter IP address: ");     
-    String host = input.next();
-    input.close();
-    int[] commonports = {20,21,22,23,25,53,80,110,143,443,445,3389,8080};
+public class portscanners extends JFrame implements ActionListener{
+    JMenuBar bar = new JMenuBar();
+    JMenu menu = new JMenu("Files");
+    JMenuItem items = new JMenuItem("Exit");
+    JLabel lab = new JLabel("Enter IP Address: ");
+     JTextField te = new JTextField(25);
+     JButton but = new JButton("Search");
+     JTextArea ar = new JTextArea();
+        JScrollPane pan = new JScrollPane(ar);
+        
+        JCheckBox ch = new JCheckBox("common ports");
+        JCheckBox ch1= new JCheckBox("udp ports");
+        JCheckBox ch2 = new JCheckBox("uncommon ports");
+       
+        int[] commonports = {20,21,22,23,25,53,80,110,143,443,445,3389,8080};
     int[] udpports = {53,67,68,123,161,162,69};
-    int[] ports = {135,137,138,139,3306,1521,5060,5061,5900,6379};
-    for(int c: ports){
+    int[] uncommonports = {135,137,138,139,3306,1521,5060,5061,5900,6379};
+        
+        public  portscanners(){
+            setTitle("Port Scanner");
+        final int width = 450;
+        final int height = 450;
+        setSize(width,height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        
+         menu.add(items);
+        bar.add(menu);
+        
+        add(lab);
+         add(te);
+         
+         add(ch);
+        ch.addActionListener(this);
+         add(ch1);
+        ch1.addActionListener(this);
+         add(ch2);
+        ch2.addActionListener(this); 
+         add(but);
+         but.setPreferredSize(new Dimension(280,20));
+         but.addActionListener(this);
+         add(pan);
+          pan.setPreferredSize(new Dimension(450,250));
+         items.addActionListener(this);
+           setVisible(true);
+        }
+   public static void main(String[] args) {
+       portscanners run = new portscanners();
+       
+}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+     Object source = e.getSource();
+    String host = te.getText();
+    
+    
+    if(source ==but){
+     if(ch.isSelected())Ports(commonports,host);
+    if(ch1.isSelected())Ports(udpports,host);
+    if(ch2.isSelected())Ports(uncommonports,host);
+}   
+    }
+
+    public String Ports(int[] ports,String host){
+      for(int c: ports){
     try(Socket s = new Socket(host,c)){
-        System.out.println("port open"+c);
+        ar.setText("port open"+c);
 }catch(Exception g){
-    
+     ar.setText("Ports are closed");
 }
     
-}
-}
+}     
+        return null;
+        
+    }
 }
